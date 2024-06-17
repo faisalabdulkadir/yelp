d8135b966391bb701eade21e3b6d21973c99aa9c
faisalabdulkadir <wizardfaisal@gmail.com>
2024-06-17 11:35:15 +0100

    configured middle ware to use cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 // const morgan = require("morgan");
 const db = require("./db");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // app.use(morgan("dev"))
