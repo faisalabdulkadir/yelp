5b9633595b66c06fc717a91d987f312897a79621
faisalabdulkadir <wizardfaisal@gmail.com>
2024-06-18 19:02:30 +0100

    minor changes

--- client/src/context/RestaurantsContext.js
+++ client/src/context/RestaurantsContext.js
@@ -4,12 +4,19 @@ export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([]);
+  const [selectedRestaurants, setSelectedRestaurants] = useState(null);
   const addRestaurant = (restaurant) => {
     setRestaurants([...restaurants, restaurant]);
   };
   return (
     <RestaurantsContext.Provider
-      value={{ restaurants, setRestaurants, addRestaurant }}
+      value={{
+        restaurants,
+        setRestaurants,
+        addRestaurant,
+        selectedRestaurants,
+        setSelectedRestaurants,
+      }}
     >
       {props.children}
     </RestaurantsContext.Provider>
