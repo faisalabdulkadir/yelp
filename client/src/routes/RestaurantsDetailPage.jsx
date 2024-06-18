ae757c972d2257273941de18328301f750594c15
faisalabdulkadir <wizardfaisal@gmail.com>
2024-06-18 19:03:04 +0100

    connecting to the details page

--- client/src/routes/RestaurantsDetailPage.jsx
+++ client/src/routes/RestaurantsDetailPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect } from "react";
+import { RestaurantsContext } from "../context/RestaurantsContext";
+import RestaurantFinder from "../apis/RestaurantFinder";
+import { useParams } from "react-router-dom";
 
 const RestaurantsDetailPage = () => {
-  return (
-    <div>RestaurantsDetailPage</div>
-  )
-}
+  const { selectedRestaurants, setSelectedRestaurants } =
+    useContext(RestaurantsContext);
+  const { id } = useParams();
 
-export default RestaurantsDetailPage
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get(`/${id}`);
+        setSelectedRestaurants(response.data.data.restaurants);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+  return <div>{selectedRestaurants && selectedRestaurants.name}</div>;
+};
+
+export default RestaurantsDetailPage;
