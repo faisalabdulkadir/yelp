743fbc61dceb361c6c16edb38e7137cd06fa8383
faisalabdulkadir <wizardfaisal@gmail.com>
2024-06-19 11:23:34 +0100

    fetching review from the backend

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,61 +1,24 @@
 import React from "react";
 import StarRating from "./StarRating";
 
-const Reviews = () => {
+const Reviews = ({ reviews }) => {
   return (
     <div className="row row-cols-3 mb-2">
-      <div className="card text-bg-primary mb-3 me-4" style={{maxWidth: "30%"}}>
-        <div className="card-header">Joan</div>
-        <div className="card-body">
-          <h5 className="card-title">
-            <StarRating rating={3.5} />
-          </h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
+      {reviews.map((review) => (
+        <div
+          key={review.id}
+          className="card text-bg-primary mb-3 me-4"
+          style={{ maxWidth: "30%" }}
+        >
+          <div className="card-header">{review.name}</div>
+          <div className="card-body">
+            <h5 className="card-title">
+              <StarRating rating={review.rating} />
+            </h5>
+            <p className="card-text">{review.review}</p>
+          </div>
         </div>
-      </div>
-
-      <div className="card text-bg-primary mb-3 me-4" style={{maxWidth: "30%"}}>
-        <div className="card-header">Joan</div>
-        <div className="card-body">
-          <h5 className="card-title">
-            <StarRating rating={3} />
-          </h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div>
-
-      <div className="card text-bg-primary mb-3 me-4" style={{maxWidth: "30%"}}>
-        <div className="card-header">Joan</div>
-        <div className="card-body">
-          <h5 className="card-title">
-            <StarRating rating={3} />
-          </h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div>
-
-      <div className="card text-bg-primary mb-3 me-4" style={{maxWidth: "30%"}}>
-        <div className="card-header">Joan</div>
-        <div className="card-body">
-          <h5 className="card-title">
-            <StarRating rating={3} />
-          </h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-      </div>
-
+      ))}
     </div>
   );
 };
