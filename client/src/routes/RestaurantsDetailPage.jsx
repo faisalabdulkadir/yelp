47080ed0e1ce19ddd90fac6ac9f5f9dafebd78dd
faisalabdulkadir <wizardfaisal@gmail.com>
2024-06-19 11:24:00 +0100

    Minor tweek to the details page

--- client/src/routes/RestaurantsDetailPage.jsx
+++ client/src/routes/RestaurantsDetailPage.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from "react";
 import { RestaurantsContext } from "../context/RestaurantsContext";
 import RestaurantFinder from "../apis/RestaurantFinder";
 import { useParams } from "react-router-dom";
+import Reviews from "../components/Reviews";
+import AddReview from "../components/AddReview";
 
 const RestaurantsDetailPage = () => {
   const { selectedRestaurants, setSelectedRestaurants } =
@@ -12,14 +14,22 @@ const RestaurantsDetailPage = () => {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
-        setSelectedRestaurants(response.data.data.restaurants);
+        setSelectedRestaurants(response.data.data);
       } catch (error) {
         console.log(error);
       }
     };
     fetchData();
   }, []);
-  return <div>{selectedRestaurants && selectedRestaurants.name}</div>;
+  return <div>{selectedRestaurants && (
+    <>
+    <h1 className="fw-lighter fs-1 text-center">{selectedRestaurants.restaurants.name}</h1>
+    <div className="mt-4">
+      <Reviews reviews={selectedRestaurants.review}/>
+    </div>
+    <AddReview />
+    </>
+  )}</div>;
 };
 
 export default RestaurantsDetailPage;
